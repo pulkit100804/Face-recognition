e6e372841e139ed09f4637d489c7c7ac35509db2
Pulkit Shrivastava <pulkit.shrivastava04@gmail.com>
2024-05-19 15:01:46 +0530

    added database to graphics

--- Datatodatabase.py
+++ Datatodatabase.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-
-
 cred = credentials.Certificate("C:\\Users\\Asus\OneDrive\Desktop\\Pulkit_AIML\\Machine learning Train\\Facial Recognition\\face-recog-attendance-6c441-firebase-adminsdk-q505x-43239c714b.json")
 firebase_admin.initialize_app(cred,{
     'databaseURL': "https://face-recog-attendance-6c441-default-rtdb.firebaseio.com/"
@@ -14,7 +11,7 @@ data = {
         {
             "Name":"123456",
             "class":"4",
-            "Back":"1",
+            "Major":"AI/ML",
             "Attendnace":"4",
             "Current":"2023-18-05 00:54:00"  
         },
@@ -22,7 +19,7 @@ data = {
         {
             "Name":"Pulkit",
             "class":"1",
-            "Back":"0",
+            "Major":"AIML",
             "Attendnace":"10",
             "Current":"2023-18-05 00:54:00"  
         },
@@ -30,7 +27,7 @@ data = {
         {
             "Name":"Elon Musk",
             "class":"1",
-            "Back":"5",
+            "Major":"AIML",
             "Attendnace":"5",
             "Current":"2023-18-05 00:54:00"  
         },
@@ -38,7 +35,7 @@ data = {
         {
             "Name":"amithabh",
             "class":"1",
-            "Back":"1",
+            "Major":"Acting",
             "Attendnace":"9",
             "Current":"2023-18-05 00:54:00"  
         },
@@ -46,7 +43,7 @@ data = {
         {
             "Name":"Prashansa",
             "class":"1",
-            "Back":"1",
+            "Major":"Padhi Likhi Gawar",
             "Attendnace":"4",
             "Current":"2023-18-05 00:54:00"  
         },
@@ -54,73 +51,10 @@ data = {
         {
             "Name":"cdes",
             "class":"4",
-            "Back":"1",
+            "Major":"Pata nai",
             "Attendnace":"4",
             "Current":"2023-18-05 00:54:00"  
         }
 }
 for key,value in data.items():
-=======
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-
-
-cred = credentials.Certificate("C:\\Users\\Asus\OneDrive\Desktop\\Pulkit_AIML\\Machine learning Train\\Facial Recognition\\face-recog-attendance-6c441-firebase-adminsdk-q505x-43239c714b.json")
-firebase_admin.initialize_app(cred,{
-    'databaseURL': "https://face-recog-attendance-6c441-default-rtdb.firebaseio.com/"
-})
-ref = db.reference('Students')
-data = {
-    "321654":
-        {
-            "Name":"123456",
-            "class":"4",
-            "Back":"1",
-            "Attendnace":"4",
-            "Current":"2023-18-05 00:54:00"  
-        },
-    "452996":
-        {
-            "Name":"Pulkit",
-            "class":"1",
-            "Back":"0",
-            "Attendnace":"10",
-            "Current":"2023-18-05 00:54:00"  
-        },
-    "452997":
-        {
-            "Name":"Elon Musk",
-            "class":"1",
-            "Back":"5",
-            "Attendnace":"5",
-            "Current":"2023-18-05 00:54:00"  
-        },
-    "452998":
-        {
-            "Name":"amithabh",
-            "class":"1",
-            "Back":"1",
-            "Attendnace":"9",
-            "Current":"2023-18-05 00:54:00"  
-        },
-    "452999":
-        {
-            "Name":"Prashansa",
-            "class":"1",
-            "Back":"1",
-            "Attendnace":"4",
-            "Current":"2023-18-05 00:54:00"  
-        },
-    "852741":
-        {
-            "Name":"cdes",
-            "class":"4",
-            "Back":"1",
-            "Attendnace":"4",
-            "Current":"2023-18-05 00:54:00"  
-        }
-}
-for key,value in data.items():
->>>>>>> 6968304016c562255eacb45aaf87a7bc68d50d5e
     ref.child(key).set(value)
--- Facialrecog.py
+++ Facialrecog.py
@@ -3,6 +3,21 @@ import os
 import pickle
 import face_recognition
 import numpy as np
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("C:\\Users\\Asus\OneDrive\Desktop\\Pulkit_AIML\\Machine learning Train\\Facial Recognition\\face-recog-attendance-6c441-firebase-adminsdk-q505x-43239c714b.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://face-recog-attendance-6c441-default-rtdb.firebaseio.com/",
+    'storageBucket': "face-recog-attendance-6c441.appspot.com"
+})
+bucket=storage.bucket()
+modetype=0
+counter=0
+ids=-1
+imgst=[]
 imgbg=cv2.imread('Resources\\background.png')
 folderpath='Resources\\Modes'
 modepath=os.listdir(folderpath)
@@ -29,7 +44,7 @@ while True:
     faceCurrent=face_recognition.face_locations(imgS)
     CurEncode=face_recognition.face_encodings(imgS,faceCurrent)
     imgbg[162:162+480,55:55+640]=img
-    imgbg[44:44+633,808:808+414]=imgmode[1]
+    imgbg[44:44+633,808:808+414]=imgmode[modetype]
     for EncoFace, Faceloc in zip(CurEncode,faceCurrent):
         matches=face_recognition.compare_faces(encode,EncoFace)
         facedis=face_recognition.face_distance(encode,EncoFace)
@@ -43,10 +58,27 @@ while True:
                 break
 
         if matched_index is not None:
-            print("Known face detected")
-            print(Stid[matched_index])
-        else:
-            print("Unknown face detected")
+            ids=Stid[matched_index]
+            if counter == 0:
+                counter =1
+                modetype=1
+    if counter != 0:
+        if counter == 1:
+            studentinfo=db.reference(f'Students/{ids}').get()
+            cv2.putText(imgbg,str(studentinfo['Attendnace']),(861,125),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
+            cv2.putText(imgbg,str(studentinfo['Major']),(1006,550),
+                        cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+            cv2.putText(imgbg,str(ids),(1006,493),
+                        cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+            cv2.putText(imgbg,str(studentinfo['class']),(1025,625),
+                        cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
+            (w,h),_= cv2.getTextSize(studentinfo['Name'],cv2.FONT_HERSHEY_COMPLEX,1,1)
+            offest=(414-w)//2
+            cv2.putText(imgbg,str(studentinfo['Name']),(808+offest,465),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(55,55,55),1)
+            
+        counter=+1
 
     cv2.imshow("face attendance", imgbg)
     if cv2.waitKey(1) & 0xFF == ord('e'):
